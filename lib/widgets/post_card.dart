c9e71b790dd85e8d67b12d5156cf6221f73d3ad3
Pierluca Lippi <lippipierluca@gmail.com>
2023-02-15 10:58:18 +0100

    feat: first commit

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,235 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter/models/user.dart' as model;
+import 'package:instagram_clone_flutter/providers/user_provider.dart';
+import 'package:instagram_clone_flutter/resources/firestore_methods.dart';
+import 'package:instagram_clone_flutter/screens/comments_screen.dart';
+import 'package:instagram_clone_flutter/utils/colors.dart';
+import 'package:instagram_clone_flutter/utils/global_variable.dart';
+import 'package:instagram_clone_flutter/utils/profile_avatar.dart';
+import 'package:instagram_clone_flutter/utils/utils.dart';
+import 'package:instagram_clone_flutter/widgets/like_animation.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class PostCard extends StatefulWidget {
+  final snap;
+  const PostCard({
+    Key? key,
+    required this.snap,
+  }) : super(key: key);
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  int commentLen = 0;
+  bool isLikeAnimating = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCommentLen();
+  }
+
+  fetchCommentLen() async {
+    try {
+      QuerySnapshot snap = await FirebaseFirestore.instance
+          .collection('posts')
+          .doc(widget.snap['postId'])
+          .collection('comments')
+          .get();
+      commentLen = snap.docs.length;
+    } catch (err) {
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
+    setState(() {});
+  }
+
+  deletePost(String postId) async {
+    try {
+      await FireStoreMethods().deletePost(postId);
+    } catch (err) {
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model.User user = Provider.of<UserProvider>(context).getUser;
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      // boundary needed for web
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: width > webScreenSize ? secondaryColor : mobileBackgroundColor,
+        ),
+        color: mobileBackgroundColor,
+      ),
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // IMAGE SECTION OF THE POST
+          Stack(
+            alignment: Alignment.center,
+            clipBehavior: Clip.none,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.35,
+                width: double.infinity,
+                child: Image.network(
+                  widget.snap['postUrl'].toString(),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              // HEADER SECTION OF THE POST
+              Positioned(
+                top: -40,
+                child: Column(
+                  children: <Widget>[
+                    ProfileAvatar(
+                      url: widget.snap['profImage'],
+                      radius: 40,
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: isLikeAnimating ? 1 : 0,
+                child: LikeAnimation(
+                  isAnimating: isLikeAnimating,
+                  child: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                  duration: const Duration(
+                    milliseconds: 400,
+                  ),
+                  onEnd: () {
+                    setState(() {
+                      isLikeAnimating = false;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          // LIKE, COMMENT SECTION OF THE POST
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: [
+                    LikeAnimation(
+                      isAnimating: widget.snap['likes'].contains(user.uid),
+                      smallLike: true,
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: widget.snap['likes'].contains(user.uid)
+                            ? const Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )
+                            : const Icon(
+                                Icons.favorite_border,
+                              ),
+                        onPressed: () => FireStoreMethods().likePost(
+                          widget.snap['postId'].toString(),
+                          user.uid,
+                          widget.snap['likes'],
+                        ),
+                      ),
+                    ),
+                    DefaultTextStyle(
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall!
+                          .copyWith(fontWeight: FontWeight.w800),
+                      child: Text(
+                        '${widget.snap['likes'].length} likes',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    )
+                  ],
+                ),
+                //DESCRIPTION AND NUMBER OF COMMENTS
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(
+                          top: 8,
+                        ),
+                        child: RichText(
+                          text: TextSpan(
+                            style: const TextStyle(color: primaryColor),
+                            children: [
+                              TextSpan(
+                                text: widget.snap['username'].toString(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' ${widget.snap['description']}',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        child: Container(
+                          child: Text(
+                            'View all $commentLen comments',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: secondaryColor,
+                            ),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                        ),
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => CommentsScreen(
+                              postId: widget.snap['postId'].toString(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          DateFormat.yMMMd()
+                              .format(widget.snap['datePublished'].toDate()),
+                          style: const TextStyle(
+                            color: secondaryColor,
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
